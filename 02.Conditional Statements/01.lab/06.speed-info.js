98b08d6816bb68872230297f35e609ca4394dbbb
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-03-02 19:20:10 +0200

    Create 06.speed-info.js

---
+++ 02.Conditional Statements/01.lab/06.speed-info.js
@@ -0,0 +1,17 @@
+function solve(input) {
+    let currentSpeed = Number(input[0]);
+
+    if (currentSpeed <= 10) {
+        console.log('slow');
+    } else if (currentSpeed <= 50) {
+        console.log('average');
+    } else if (currentSpeed <= 150) {
+        console.log('fast');
+    } else if (currentSpeed <= 1000) {
+        console.log('ultra fast');
+    } else {
+        console.log('extremely fast');
+    }
+}
+
+solve((["49.5"]))
