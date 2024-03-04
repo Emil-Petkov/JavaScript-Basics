b87d0df46e2e79fc4f95501f812116e4e9865638
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-03-04 20:50:01 +0200

    Create 06.number-in-range.js

---
+++ 03.Conditional Statements Advanced/01.lab/06.number-in-range.js
@@ -0,0 +1,11 @@
+function solve(input) {
+    let number = Number(input[0]);
+
+    if (number !== 0 && number >= -100 && number <= 100) {
+        console.log('Yes');
+    } else {
+        console.log('No');
+    }
+}
+
+solve(["0"]);
