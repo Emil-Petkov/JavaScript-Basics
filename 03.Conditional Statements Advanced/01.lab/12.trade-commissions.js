a57242dedde6c518225a71e4c5a284e17f6b0525
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-03-04 22:26:30 +0200

    Create 12.trade-commissions.js

---
+++ 03.Conditional Statements Advanced/01.lab/12.trade-commissions.js
@@ -0,0 +1,55 @@
+function solve(input) {
+    let city = input[0];
+    let sales = Number(input[1]);
+
+    if (sales < 0) {
+        console.log('error');
+        return;
+    }
+
+    let commission = 0;
+    switch (city) {
+        case 'Sofia':
+            if (sales <= 500) {
+                commission = 0.05;
+            } else if (sales <= 1000) {
+                commission = 0.07;
+            } else if (sales <= 10000) {
+                commission = 0.08;
+            } else {
+                commission = 0.12;
+            }
+            break;
+        case 'Varna':
+            if (sales <= 500) {
+                commission = 0.045;
+            } else if (sales <= 1000) {
+                commission = 0.075;
+            } else if (sales <= 10000) {
+                commission = 0.10;
+            } else {
+                commission = 0.13;
+            }
+            break;
+        case 'Plovdiv':
+            if (sales <= 500) {
+                commission = 0.055;
+            } else if (sales <= 1000) {
+                commission = 0.08;
+            } else if (sales <= 10000) {
+                commission = 0.12;
+            } else {
+                commission = 0.145;
+            }
+            break;
+        default:
+            console.log('error');
+            return;
+    }
+
+    let total = sales * commission;
+    console.log(total.toFixed(2));
+}
+
+
+solve(["Sofia", "1500"]);
