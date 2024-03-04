8ab38c2fb97cd8ec6a57b5a9082f730d3904b718
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-03-04 20:30:05 +0200

    Create 04.personal-titles.js

---
+++ 03.Conditional Statements Advanced/01.lab/04.personal-titles.js
@@ -0,0 +1,24 @@
+function solve(input) {
+    let age = Number(input[0]);
+    let gender = input[1];
+
+    switch (gender) {
+        case 'm':
+            if (age < 16) {
+                console.log('Master');
+            } else if (age >= 16) {
+                console.log('Mr.');
+            }
+            break;
+
+        case 'f':
+            if (age < 16) {
+                console.log('Miss');
+            } else if (age >= 16) {
+                console.log('Ms.');
+            }
+            break;
+    }
+}
+
+solve((["12", "f"]));
